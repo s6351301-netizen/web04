0a2b4da7db23370ce62a027167044ad204bcfc65
may <s6351301@gmail.com>
2026-07-21 10:30:22 +0800

    先建立註冊,會員登入及管理登入頁面

---
+++ front/admin.php
@@ -0,0 +1,15 @@
+<table class="all">
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp"><input type="text" name="acc" id="acc"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="text" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">驗證碼</td>
+        <td class="pp"><input type="text" name="code" id="code"></td>
+    </tr>
+</table>
+<div class='ct'><button>確認</button></div>
