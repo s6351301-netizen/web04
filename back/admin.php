b507f9a2e966c4565fc2aac808bd3508a8f7dc57
may <s6351301@gmail.com>
2026-07-21 14:30:30 +0800

    建立管理員管理的列表,新增,修改三個頁面版型

--- back/admin.php
+++ back/admin.php
@@ -1,1 +1,15 @@
-admin
+<div class="ct">
+    <button onclick="location.href='?do=add_admin'">新增管理員</button>
+</div>
+<table class="all">
+    <tr class="tt ct">
+        <td>帳號</td>
+        <td>密碼</td>
+        <td>管理</td>
+    </tr>
+    <tr class="pp ct">
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
