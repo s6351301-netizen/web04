8c793ca4adbcf62e8b91234d6f48a970bfdc6c9f
may <s6351301@gmail.com>
2026-07-24 11:27:24 +0800

    更新商品資料表

--- front/main.php
+++ front/main.php
@@ -1,19 +1,29 @@
 <?php 
 $type_id=$_GET['type']??0;
 $nav_str="全部商品";
+
+$items=$Item->all(['sh'=>1]);
+
 if($type_id!=0){
  $type=$Type->find($type_id);
  if($type['main_id']==0){ //是大分類
     $nav_str=$type['name'];
 
+    $items=$Item->all(['sh'=>1,'big'=>$type['id']]);
+
  }else{  //是中分類
     //取出大分類
     $big=$Type->find($type['main_id']);
     $nav_str=$big['name']. " > ".$type['name'];
+
+    $items=$Item->all(['sh'=>1,'mid'=>$type['id']]);
  }
 }
+
+
 
 
 ?>
 
 <h2><?= $nav_str ?></h2>
+<?php dd($items);?>
